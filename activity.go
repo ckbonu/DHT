cafc7904453001e9a6a8d8167f92c29203209836
Krishna Chaitanya <krishna.c@isteer.com>
2018-12-04 22:16:43 +0530

    Add files via upload

--- activity.go
+++ activity.go
@@ -1,11 +1,10 @@
 package dhtxx
 
 import (
-	"fmt"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
-	 "github.com/d2r2/go-dht"
+	"github.com/d2r2/go-dht"
 )
 
 var log = logger.GetLogger("activity-dhtxx")
@@ -13,6 +12,7 @@ var log = logger.GetLogger("activity-dhtxx")
 const (
 
 	ivPinNumber  = "PinNumber"
+	ovResult = "output"
 )
 
 // MyActivity is a stub for your Activity implementation
@@ -36,6 +36,8 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	//sensorType := context.GetInput(ivsensorType).(string)
 
 	Pin := context.GetInput(ivPinNumber).(int)
+	//output := context.GetInput(ivoutput).(string)
+
 	// do eval
 	temperature, humidity, retried, err :=
 		dht.ReadDHTxxWithRetry(dht.DHT11, Pin, true, 10)
@@ -43,8 +45,12 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 		log.Debug(err)
 	}
 	// Print temperature and humidity
-	fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-		temperature, humidity, retried)
+    context.SetOutput(ovResult, temperature)
+	context.SetOutput(ovResult, humidity)
+	context.SetOutput(ovResult, retried)
+
+	// fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
+		// temperature, humidity, retried)
 
 	return true, nil
 }
