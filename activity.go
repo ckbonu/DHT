39f7e9aba1fd345c0666e3506779effa42d512f6
Krishna Chaitanya <krishna.c@isteer.com>
2018-12-10 14:08:52 +0530

    Add files via upload

--- activity.go
+++ activity.go
@@ -1,10 +1,9 @@
 package dhtxx
 
 import (
-
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
-	"github.com/d2r2/go-dht"
+	"github.com/MichaelS11/go-dht"
 )
 
 var log = logger.GetLogger("activity-dhtxx")
@@ -35,22 +34,25 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	//sensorType := context.GetInput(ivsensorType).(string)
 
-	Pin := context.GetInput(ivPinNumber).(int)
+	//Pin := context.GetInput(ivPinNumber).(int)
 	//output := context.GetInput(ivoutput).(string)
 
-	// do eval
-	temperature, humidity, retried, err :=
-		dht.ReadDHTxxWithRetry(dht.DHT11, Pin, true, 10)
+	dht, err := dht.NewDHT("GPIO17", dht.Fahrenheit, "")
 	if err != nil {
-		log.Debug(err)
+		context.SetOutput(ovResult, err)
+		return false, err
 	}
+
+	humidity, temperature, err := dht.Read()
+	if err != nil {
+		context.SetOutput(ovResult, err)
+		return false, err
+	}
+	// do eval
+
 	// Print temperature and humidity
     context.SetOutput(ovResult, temperature)
 	context.SetOutput(ovResult, humidity)
-	context.SetOutput(ovResult, retried)
-
-	// fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-		// temperature, humidity, retried)
 
 	return true, nil
 }
